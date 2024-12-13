a97e84c1bb28c6b02cb66ff6a3cc04b6b85a887c
Radhika <bugbox.97@gmail.com>
2024-12-13 12:10:43 +0100

    add user components, setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from "./components/user-list/user-list.component";
+import { UserDetailsComponent } from "./components/user-details/user-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserListComponent },
+  { path: ':id/view', component: UserDetailsComponent },
+  { path: ':id/edit', component: UserDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent,
   ],
   imports: [
     BrowserModule,
